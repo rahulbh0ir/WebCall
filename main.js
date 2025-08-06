4d563b3dc55d3998333daa8511a751bdb36f7d64
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-08-06 16:26:11 +0530

    up

---
+++ main.js
@@ -0,0 +1,80 @@
+
+    const socket = new WebSocket("ws://localhost:3000");
+
+
+    let pc = null;
+    let room = null;
+
+    const config = {
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+    };
+
+    const localVideo = document.getElementById("localVideo");
+    const remoteVideo = document.getElementById("remoteVideo");
+
+    let localStream;
+
+    socket.onmessage = async (event) => {
+      const msg = JSON.parse(event.data);
+
+      if (msg.type === "welcome") {
+        console.log("Connected. My ID:", msg.id);
+      } else if (msg.type === "offer") {
+        await createPeerConnection();
+        await pc.setRemoteDescription(new RTCSessionDescription(msg.payload));
+        const answer = await pc.createAnswer();
+        await pc.setLocalDescription(answer);
+        send("answer", pc.localDescription);
+      } else if (msg.type === "answer") {
+        await pc.setRemoteDescription(new RTCSessionDescription(msg.payload));
+      } else if (msg.type === "ice-candidate") {
+        if (msg.payload) {
+          await pc.addIceCandidate(msg.payload);
+        }
+      }
+    };
+
+    function send(type, payload) {
+      socket.send(JSON.stringify({ type, room, payload }));
+    }
+
+
+
+    async function joinRoom() {
+
+      document.getElementById("video").style.display = "grid";
+
+      room = document.getElementById("roomInput").value.trim();
+      if (!room) return alert("Enter a room name");
+
+      socket.send(JSON.stringify({ type: "join", room }));
+
+      await createPeerConnection();
+
+      const offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+      send("offer", pc.localDescription);
+    }
+
+    
+    
+    
+    async function createPeerConnection() {
+      if (pc) return;
+
+      pc = new RTCPeerConnection(config);
+
+      pc.onicecandidate = (event) => {
+        if (event.candidate) {
+          send("ice-candidate", event.candidate);
+        }
+      };
+
+      pc.ontrack = (event) => {
+        remoteVideo.srcObject = event.streams[0];
+      };
+
+      localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
+      localVideo.srcObject = localStream;
+    }
